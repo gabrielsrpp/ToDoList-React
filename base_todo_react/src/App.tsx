376f53b5af72302e3ba1aada34a54ac6711a3a79
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-24 14:27:00 -0300

    Criando formulario

--- base_todo_react/src/App.tsx
+++ base_todo_react/src/App.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 // css 
 import styles from './App.module.css';
@@ -11,10 +13,23 @@ function App() {
   return (
     <div>
       <Header />
-      <main className={styles.main}/>
+      
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText='Criar tarefa'/>
+        </div>
+
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList/>
+        </div>
+      </main>
+      
       <Footer />
     </div>
   );
 }
+
 
 export default App;
