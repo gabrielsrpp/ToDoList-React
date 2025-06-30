913ad70cabb7c57c0559e04632bfe3bf8648f51e
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-30 10:36:29 -0300

    Finalizando lista

--- base_todo_react/src/components/TaskList.tsx
+++ base_todo_react/src/components/TaskList.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 // interfaces
 import { ITask } from '../interfaces/Task'
 // CSS
-import styles from '../TaskList.module.css'
+import styles from './TaskList.module.css'
 
 
 interface Props  {
@@ -19,21 +19,21 @@ const TaskList = (props: Props) => {
   return (
     <>
       {props.taskList.length > 0 ? (
-        props.taskList.map((task) => (
-          <div key={task.id}>
-            <div>
-              <h4>{task.title}</h4>
-              <p>Dificuldade: {task.difficulty}</p>
-            </div>
-            <div>
-              <i className="bi bi-pencil"></i>
-              <i className="bi bi-trash"></i>
-            </div>
-          </div>
-        ))
-      ) : (
-        <p>Não há tarefas cadastradas</p>
-      )}
+  props.taskList.map((task) => (
+    <div key={task.id} className={styles.task}>
+      <div className={styles.details}>
+        <h4>{task.title}</h4>
+        <p>Dificuldade: {task.difficulty}</p>
+      </div>
+      <div className={styles.actions}>
+        <i className="bi bi-pencil"></i>
+        <i className="bi bi-trash"></i>
+      </div>
+    </div>
+  ))
+) : (
+  <p>Não há tarefas cadastradas</p>
+)}
     </>
   );
 };
