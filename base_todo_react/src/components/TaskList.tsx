657064a00bf442cbfc7ce9ad96f237f2762e0e8e
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-30 09:38:44 -0300

    HTML dos itens da lista

--- base_todo_react/src/components/TaskList.tsx
+++ base_todo_react/src/components/TaskList.tsx
@@ -21,7 +21,14 @@ const TaskList = (props: Props) => {
       {props.taskList.length > 0 ? (
         props.taskList.map((task) => (
           <div key={task.id}>
-            <p>{task.title}</p>
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
           </div>
         ))
       ) : (
