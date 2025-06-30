8b9383a50dd3ce4adb55b6fa6e259a1c22fea2a4
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-06-30 10:49:34 -0300

    Removendo tarefas

--- base_todo_react/src/components/TaskList.tsx
+++ base_todo_react/src/components/TaskList.tsx
@@ -9,17 +9,18 @@ import styles from './TaskList.module.css'
 
 interface Props  {
   taskList: ITask [];
+  handleDelete(id: number): void
 }
 
 interface Props {
   taskList: ITask[];
 }
 
-const TaskList = (props: Props) => {
+const TaskList = ({taskList, handleDelete}:Props) => {
   return (
     <>
-      {props.taskList.length > 0 ? (
-  props.taskList.map((task) => (
+      {taskList.length > 0 ? (
+      taskList.map((task) => (
     <div key={task.id} className={styles.task}>
       <div className={styles.details}>
         <h4>{task.title}</h4>
@@ -27,7 +28,10 @@ const TaskList = (props: Props) => {
       </div>
       <div className={styles.actions}>
         <i className="bi bi-pencil"></i>
-        <i className="bi bi-trash"></i>
+        <i className="bi bi-trash" 
+        onClick={() => {handleDelete(task.id)
+          
+        }}></i>
       </div>
     </div>
   ))
